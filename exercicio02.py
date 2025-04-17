87d7e519558e64d528acbf586f405ed667d55f0b
Cauã Souza <cauasouza3112@gmail.com>
2025-04-17 14:29:47 -0300

    Exercicio se é par ou impar e se é - ou +

--- exercicio02.py
+++ exercicio02.py
@@ -1,12 +1,16 @@
-alg=int(input("Informe um valor: "))
-if alg % 2 == 0:
-    if alg<0:
-        print(f"{alg} é par e é positivo")
-    else:
-        print(f"{alg} é impar e é positivo")
-else:
-    if alg%2==0:
-        print(f"{alg} é par e é neagtivo")
-    else:
-        print(f"{alg} é impar e é negativo")
+resp='s'
+while resp=='s':
+    alg=int(input("Informe um valor: "))
 
+    if alg % 2 == 0:
+        if alg<0:
+            print(f"{alg} é par e é positivo")
+        else:
+            print(f"{alg} é impar e é positivo")
+    else:
+        if alg%2==0:
+            print(f"{alg} é par e é neagtivo")
+        else:
+            print(f"{alg} é impar e é negativo")
+
+    resp=input("Deseja fazer o calculo novamente (s/n): ")
