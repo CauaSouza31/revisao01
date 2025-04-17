fcf24a1dc705a350b1d518845f36198e34368f0a
Cauã Souza <cauasouza3112@gmail.com>
2025-04-17 13:59:28 -0300

    Exercicio de valor A, B e C

---
+++ exercicio01.py
@@ -0,0 +1,10 @@
+v1=int(input("Informe o primeiro valor: "))
+v2=int(input("Informe o segundo valor: "))
+v3=int(input("Informe o terceiro valor: "))
+
+soma = v1 + v2
+print(soma)
+if soma> v3:
+    print("soma maior que v3")
+else:
+    print("soma menor que v3")
